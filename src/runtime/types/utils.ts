7a34fd89fef816870407f5900e47540cb5fea3c3
Radu Margina <ip.radu@gmail.com>
2025-07-30 12:33:13 +0300

    fix: refine `NestedKeyOf` type to improve accuracy and edge case handling like items.0.itemField

--- src/runtime/types/utils.ts
+++ src/runtime/types/utils.ts
@@ -16,22 +16,22 @@ interface HasConstructor {
   new (...args: unknown[]): unknown
 }
 
-export type NestedKeyOf<T> = T extends Record<infer Key, unknown>
+export type NestedKeyOf<T> = T extends Record<string | number, unknown>
   ? T extends HasConstructor
     ? never
     : T extends CallableFunction
       ? never
-      : Key extends string | number
-        ? (ObjectKeys<T> | (T[Key] extends object
-            ? `${ObjectKeys<Pick<T, Key>>}.${NestedKeyOf<T[Key]>}`
-            : T extends unknown[]
-              ? T extends [unknown, ...unknown[]]
-                ? never
-                : T[number] extends object
-                  ? `${number}.${NestedKeyOf<T[number]>}`
-                  : never
-              : never))
-        : never
+      : {
+        [K in keyof T & (string | number)]:
+        | `${K}`
+        | (T[K] extends (infer U)[]
+        ? U extends object
+          ? `${K}.${number}` | `${K}.${number}.${NestedKeyOf<U>}`
+          : `${K}.${number}`
+        : T[K] extends object
+          ? `${K}.${NestedKeyOf<T[K]>}`
+          : never)
+      }[keyof T & (string | number)]
   : never
 
 type SingleDigit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
